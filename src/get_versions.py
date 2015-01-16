7b2c80dfee58f078702e7bf1f0478b1679e252b3
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-16 13:43:04 -0500

    Removed unittest I accidently put in src/get_versions.py

--- src/get_versions.py
+++ src/get_versions.py
@@ -95,19 +95,4 @@ def rep_by_pep440(ver):
     ver["version"] = git2pep440(ver["version"])
     return ver
 
-class PEP440Versions(unittest.TestCase):
-    def check_git_ver_str(git_ver_str, exp_pep440_str):
-        self.assertEqual(git2pep440('git_ver_str'), exp_pep440_str)
 
-    def test_tag():
-        self.check_git_ver_str('0.2.1', '0.2.1')
-
-    def test_tag_dirty():
-        self.check_git_ver_str('0.2.1-dirty', '0.2.1.post0.dev0+dirty')
-
-    def test_commit_after_tag():
-        self.check_git_ver_str('0.2.1-4-g2g433f2', '0.2.1.post0.dev4-g2g433f2')
-
-    def test_commit_after_tag_dirty():
-        self.check_git_ver_str('0.2.1-4-g2g433f2-dirty',
-                               '0.2.1.post0.dev4-g2g433f2.dirty')
